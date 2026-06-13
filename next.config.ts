ab78f976aa748967049437cce9143f132d709a20
Huynh Phuc Thien <hpthien1307@gmail.com>
2026-06-13 19:23:51 +0700

    fix next.config.ts export syntax

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
     images: {
         unoptimized: true,
         domains: ['bagisto.monamedia.net']
     }
 };
 
-module.exports = nextConfig;
+export default nextConfig;
+
